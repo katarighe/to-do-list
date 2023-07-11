927d29206e3193d9e5c99b0eb03f99b84ec65255
katarighe <m.ighe@hotmail.com>
2023-07-11 16:58:52 +0300

    Add the "mytasks" script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './style.css';
+
+const mytasks = [
+    {
+        description: "Buy groceries",
+        completed: false,
+        index: 1
+    },
+    {
+        description: "Clean the house",
+        completed: false,
+        index: 2
+    },
+    {
+        description: "Go to the gym",
+        completed: false,
+        index: 3
+    },
+    {
+        description: "Make some breakfast",
+        completed: false,
+        index: 4
+    },
+    {
+        description: "Wash the car",
+        completed: false,
+        index: 5
+      }
+];
