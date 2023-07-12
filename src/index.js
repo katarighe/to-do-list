e661b6557f5e2d6f0e18dbae87f1cfaef799e00e
katarighe <m.ighe@hotmail.com>
2023-07-12 18:15:29 +0300

    Fix bugs and errors

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import { getmyTask, addmyTask, editmyTask, deletemyTask } from './modules/functi
 // Start
 const listGroup = document.querySelector('.to-do-group');
 const newTask = document.querySelector('.to-do-add').querySelector('input');
-const submiticon = document.querySelector('to-do-add').querySelector('j');
+const submitIcon = document.querySelector('.to-do-add').querySelector('i');
 newTask.addEventListener('keypress', (event) => addmyTask(event));
-submiticon.addEventListener('click', () => addmyTask('clicked'));
+submitIcon.addEventListener('click', () => addmyTask('clicked'));
 
 listGroup.addEventListener('click', (event) => {
   const clickedItem = event.target.classList[event.target.classList.length - 1];
   const li = event.target.parentElement;
-  if (clickedItem = 'delete-icon') deletemyTask(li.id);
+  if (clickedItem === 'delete-icon') deletemyTask(li.id);
 });
 
 listGroup.addEventListener('keypress', (event) => {
   const pressedItem = event.target.classList[event.target.classList.length - 1];
   const li = event.target.parentElement;
-  if (pressedItem = 'edit-to-do') editmyTask({ index: li.id, event });
+  if (pressedItem === 'edit-to-do') editmyTask({ index: li.id, event });
 });
 
 window.addEventListener('load', () => { getmyTask(); });
