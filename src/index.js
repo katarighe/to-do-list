0764809b8e61eebfed1e1c5dd09e709d31d44ce6
katarighe <m.ighe@hotmail.com>
2023-07-12 11:30:06 +0300

    Make typo changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './style.css';
 
-import { getmyTask } from './modules/interface.js'
+import { getmyTask } from './modules/interface.js';
 
 import { addmyTask, editmyTask, deletemyTask } from './modules/functions.js';
 
-import { changetodostatus, removecompletedtodos } from './modules/status.js';
-
+import { changeTaskStatus, removeCompletedTasks } from './modules/status.js';
