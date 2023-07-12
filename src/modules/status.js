2087f13554f93fe8042746e1730f44c680af1016
katarighe <m.ighe@hotmail.com>
2023-07-12 16:16:09 +0300

    Set up the JS code

--- src/modules/status.js
+++ src/modules/status.js
@@ -1,4 +1,24 @@
 import { getmyTask } from './interface.js';
-import { ls, setTask } from './local-storage.js'
+import { ls, setTask } from './local-storage.js';
 
 // Start
+
+const changeTaskStatus = ({ index, status }) => {
+  const myTask = ls();
+  myTask[index - 1].completed = status;
+  setTask(myTask);
+  getmyTask();
+};
+
+const removeCompletedTasks = () => {
+  const myTask = ls();
+  const uncompletedTasks = myTask.filter(({ completed }) => completed !== true);
+  const newTodos = uncompletedTasks.map((element, index) => {
+    element.index = index + 1;
+    return element;
+  });
+  setTask(newTodos);
+  getmyTask();
+};
+
+export { changeTaskStatus, removeCompletedTasks };
