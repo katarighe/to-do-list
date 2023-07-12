3b8b3e268ebba0b6f02d80a73c0d093604105c44
katarighe <m.ighe@hotmail.com>
2023-07-12 11:33:32 +0300

    Add "start" tag below

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ import { getmyTask } from './modules/interface.js';
 import { addmyTask, editmyTask, deletemyTask } from './modules/functions.js';
 
 import { changeTaskStatus, removeCompletedTasks } from './modules/status.js';
+
+// Start
