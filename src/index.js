5a344e224c01b571ef2f8601ec8bde2bc42d513a
katarighe <m.ighe@hotmail.com>
2023-07-12 16:37:35 +0300

    Add ES6 JS components on the file

--- src/index.js
+++ src/index.js
@@ -7,3 +7,27 @@ import { addmyTask, editmyTask, deletemyTask } from './modules/functions.js';
 import { changeTaskStatus, removeCompletedTasks } from './modules/status.js';
 
 // Start
+const listGroup = document.querySelector('.to-do-group');
+const newTask = document.querySelector('.to-do-add').querySelector('input');
+const submiticon = document.querySelector('to-do-add').querySelector('j');
+newTask.addEventListener('keypress', (event) => addmyTask(event));
+submiticon.addEventListener('click', () => addmyTask('clicked'));
+
+listGroup.addEventListener('click', (event) => {
+  const clickedItem = event.target.classList[event.target.classList.length - 1];
+  const li = event.target.parentElement;
+  if (clickedItem = 'delete-icon') deletemyTask(li.id);
+  if (clickedItem = 'checked-icon') changeTaskStatus({ index: li.id, status: false });
+  if (clickedItem === 'unchecked-icon') changeTaskStatus({ index: li.id, status: true });
+});
+
+listGroup.addEventListener('keypress', (event) => {
+  const pressedItem = event.target.classList[event.target.classList.length - 1];
+  const li = event.target.parentElement;
+  if (pressedItem = 'edit-to-do') editmyTask({ index: li.id, event });
+});
+
+const clearCompleted = document.querySelector('.clear-to-do');
+clearCompleted.addEventListener('click', removeCompletedTasks);
+
+window.addEventListener('load', () => { getmylist(); });
