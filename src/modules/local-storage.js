a8bf9ce82f80edc0168ed73c183dec9d84fa8ce9
katarighe <m.ighe@hotmail.com>
2023-07-12 10:51:41 +0300

    Make first changes on the JS file

---
+++ src/modules/local-storage.js
@@ -0,0 +1,9 @@
+const ls = () => 
+(JSON.parse(localStorage.getItem('myTask')) ?
+JSON.parse(localStorage.getItem('myTask')) : []);
+
+const setTask = (ml) => {
+    localStorage.setItem('myTask', JSON.stringify(ml)
+    )};
+
+export { ls, setTask };
