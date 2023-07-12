ebb276d3791775c0c2ca9c5480e787fcb535acc5
katarighe <m.ighe@hotmail.com>
2023-07-12 11:08:17 +0300

    Create ES6 JS code for local storage

--- src/modules/local-storage.js
+++ src/modules/local-storage.js
@@ -1,9 +1,8 @@
-const ls = () => 
-(JSON.parse(localStorage.getItem('myTask')) ?
-JSON.parse(localStorage.getItem('myTask')) : []);
+const ls = () => (JSON.parse(localStorage.getItem('myTask'))
+  ? JSON.parse(localStorage.getItem('myTask')) : []);
 
 const setTask = (ml) => {
-    localStorage.setItem('myTask', JSON.stringify(ml)
-    )};
+  localStorage.setItem('myTask', JSON.stringify(ml));
+};
 
 export { ls, setTask };
