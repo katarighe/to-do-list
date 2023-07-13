cec3764cb8ec7c099394e3a2ba271e5278cbbce7
katarighe <m.ighe@hotmail.com>
2023-07-13 11:15:15 +0300

    Add new function for complete/uncomplete tasks

---
+++ src/modules/taskStatus.js
@@ -0,0 +1,26 @@
+import { 
+  myTask, 
+  getmyTask, 
+  updateInterface } from './functions.js';
+
+  const changeTaskStatus = ({ index, status }) => {
+    myTask[index-1].completed = status;
+    localStorage.setItem('myTask', JSON.stringify(myTask));
+    getmyTask();
+};
+
+const removeCompletedTasks = () => {
+    const uncompletedTasks = myTask.filter((task) => task.completed !== true);
+    const newTasks = uncompletedTasks.map((task, index) => {
+        task.index = index + 1;
+        return task;
+    });
+    
+    localStorage.setItem('myTask', JSON.stringify(newTasks));
+    updateInterface(newTasks);
+};
+
+export {
+  changeTaskStatus,
+  removeCompletedTasks,
+};
