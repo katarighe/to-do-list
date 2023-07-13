a4fcd1e30bf16e3704ea5f25f08603a2845c47a4
katarighe <m.ighe@hotmail.com>
2023-07-13 10:13:02 +0300

    Add new elements for interactive list + clear list

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import {
   deletemyTask,
 } from './modules/functions.js';
 
+import {
+  changeTaskStatus,
+  // Line break here
+  removeCompletedTasks,
+} from './modules/taskStatus.js';
+
 const listGroup = document.querySelector('.to-do-group');
 const newTask = document.querySelector('.to-do-add').querySelector('input');
 const submitIcon = document.querySelector('.to-do-add').querySelector('i');
@@ -18,6 +24,8 @@ listGroup.addEventListener('click', (event) => {
   const clickedItem = event.target.classList[event.target.classList.length - 1];
   const li = event.target.parentElement;
   if (clickedItem === 'delete-icon') deletemyTask(li.id);
+  if (clickedItem === 'checked-icon') changeTaskStatus({ index: li.id, status: false });
+  if (clickedItem === 'unchecked-icon') changeTaskStatus({ index: li.id, status: true });
 });
 
 listGroup.addEventListener('keypress', (event) => {
@@ -25,5 +33,8 @@ listGroup.addEventListener('keypress', (event) => {
   const li = event.target.parentElement;
   if (pressedItem === 'edit-to-do') editmyTask({ index: li.id, event });
 });
+
+const clearCompleted = document.querySelector('.clear-to-do');
+clearCompleted.addEventListener('click', removeCompletedTasks);
 
 window.addEventListener('load', () => { getmyTask(); });
