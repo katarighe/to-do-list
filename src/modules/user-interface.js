76395bc493a98762f0bb70941840d8398ff40b7f
katarighe <m.ighe@hotmail.com>
2023-07-19 12:07:56 +0300

    Make minor changes on the tags

--- src/modules/user-interface.js
+++ src/modules/user-interface.js
@@ -7,10 +7,10 @@ const getmyTask = () => {
   const myTaskElement = myTask.map((item) => `
         <li class="to-do-list to-do-item" id=${item.index}>
           ${item.completed === true ? `
-            <i class="fa-solid fa-check checked-icon" title="Mark task as incomplete"></i>`
-    : '<i class="fa-solid fa-square unchecked-icon" title="Mark task as complete"></i>'}
-          <input type="text" class=${item.completed === true ? 'decoration edit-todo' : ' edit-todo'}  value="${item.description}" title="Edit this task">
-          <span class="edit-focus-element"></span>
+            <i class="fa-solid fa-check check-icon" title="Mark task as incomplete"></i>`
+    : '<i class="fa-solid fa-square uncheck-icon" title="Mark task as complete"></i>'}
+          <input type="text" class=${item.completed === true ? 'decoration edit-to-do' : ' edit-to-do'}  value="${item.description}" title="Edit this task">
+          <span class='edit-focus-element'></span>
           <i class="fa-solid fa-trash delete-icon" title='Remove a task'></i>
           <i class="fa-solid fa-ellipsis-vertical more-icon" title='See more'></i>
         </li>`).join('');
