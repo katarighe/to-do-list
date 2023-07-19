dd5222573219db2d2f98f62ee7c0da0de3177237
katarighe <m.ighe@hotmail.com>
2023-07-19 16:12:07 +0300

    Fix minor errors as well as ESLint

--- __test__/add-remove.test.js
+++ __test__/add-remove.test.js
@@ -5,37 +5,37 @@ import {
 
 jest.mock('../src/modules/inteface.js');
 
-describe('Add and remove testing',() => {
-    // Start for Add Item
-    test('Add testing', () => {
-        // Arrange the elements
-        document.body.innerHTML = '<div class="to-do-list to-do-add">'
-        + '<input type="text" placeholder="Add to your list..." value="new task">'
-        + '<i class="fa-solid fa-plus add-icon" value="add task"></i>'
-        + '</div>';
+describe('Add and remove testing', () => {
+  // Start for Add Item
+  test('Add testing', () => {
+    // Arrange the elements
+    document.body.innerHTML = '<div class="to-do-list to-do-add">'
+    + '<input type="text" placeholder="Add to your list..." value="new task">'
+    + '<i class="fa-solid fa-plus add-icon" value="add task"></i>'
+    + '</div>';
 
-        const myTask = JSON.parse(localStorage.getItem('myTask'));
+    const myTask = JSON.parse(localStorage.getItem('myTask'));
 
-        // Act the Add Task
-        addmyTask ('clicked');
-        const newTask = JSON.parse(localStorage.getItem('myTask'));
-        const newTaskLength = newTaskLength; 
+    // Act the Add Task
+    addmyTask('clicked');
+    const newTask = JSON.parse(localStorage.getItem('myTask'));
+    const newTaskLength = newTask.length;
 
-        // Assert the Add Task
-        expect(newTaskLength).toBe(myTask.length + 1);
-        expect(newTask[newTaskLength - 1].description).toBe('new task');
-    });
+    // Assert the Add Task
+    expect(newTaskLength).toBe(myTask.length + 1);
+    expect(newTask[newTaskLength - 1].description).toBe('new task');
+  });
 
-// Start for Remove Items
-test('Removing item', () => {
-  // Arrange the lines
-  const myTask = JSON.parse(localStorage.getItem('myTask'));
+  // Start for Remove Items
+  test('Removing item', () => {
+    // Arrange the lines
+    const myTask = JSON.parse(localStorage.getItem('myTask'));
 
-  // Act the remove task
-  deletemylist(1);
-  const newTask = JSON.parse(localStorage.getItem('myTask'));
+    // Act the remove task
+    deletemyTask(1);
+    const newTask = JSON.parse(localStorage.getItem('myTask'));
 
-  // Assert the remove task
-  expect(newTask.length).toBe(myTask.length - 1);
-});
+    // Assert the remove task
+    expect(newTask.length).toBe(myTask.length - 1);
+  });
 });
