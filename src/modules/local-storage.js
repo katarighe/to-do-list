f674641db1c0b759036fe6f6bd820bdad5bc0f62
katarighe <m.ighe@hotmail.com>
2023-07-19 11:46:47 +0300

    Separate function.js into two new JS files

---
+++ src/modules/local-storage.js
@@ -0,0 +1,10 @@
+const ls = () => (JSON.parse(localStorage.getItem('myTask')) ? JSON.parse(localStorage.getItem('myTask')) : []);
+
+const setTask = (ml) => {
+  localStorage.setItem('myTask', JSON.stringify(ml));
+};
+
+export {
+  ls,
+  setTask
+};
