1e1b5ed68cbe6cfe72963b552050b068aa6d2db5
katarighe <m.ighe@hotmail.com>
2023-07-19 12:11:48 +0300

    Minor changes on the design

--- src/modules/user-interface.js
+++ src/modules/user-interface.js
@@ -7,9 +7,9 @@ const getmyTask = () => {
   const myTaskElement = myTask.map((item) => `
         <li class="to-do-list to-do-item" id=${item.index}>
           ${item.completed === true ? `
-            <i class="fa-solid fa-check check-icon" title="Mark task as incomplete"></i>`
+            <i class="fa-solid fa-square-check check-icon" title="Mark task as incomplete"></i>`
     : '<i class="fa-solid fa-square uncheck-icon" title="Mark task as complete"></i>'}
-          <input type="text" class=${item.completed === true ? 'decoration edit-to-do' : ' edit-to-do'}  value="${item.description}" title="Edit this task">
+          <input type="text" class=${item.completed === true ? 'decoration edit-to-do' : ' edit-to-do'} value="${item.description}" title="Edit this task">
           <span class='edit-focus-element'></span>
           <i class="fa-solid fa-trash delete-icon" title='Remove a task'></i>
           <i class="fa-solid fa-ellipsis-vertical more-icon" title='See more'></i>
