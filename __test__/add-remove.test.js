73aa299938e6d150f2b1e1c47302549f3bb40857
katarighe <m.ighe@hotmail.com>
2023-07-19 17:12:59 +0300

    Make minor changes

--- __test__/add-remove.test.js
+++ __test__/add-remove.test.js
@@ -2,9 +2,10 @@ import { addmyTask, deletemyTask } from '../src/modules/functions.js';
 jest.mock('../src/modules/user-interface.js');
 
 describe('Add and remove testing', () => {
-  // Start for Add Item
+  // Start
   test('Add testing', () => {
-    // Arrange the elements
+
+    // Arrange (Add)
     document.body.innerHTML = '<div class="to-do-list to-do-add">'
     + '<input type="text" placeholder="Add to your list..." value="new task">'
     + '<i class="fa-solid fa-plus add-icon" value="add task"></i>'
@@ -12,26 +13,26 @@ describe('Add and remove testing', () => {
 
     const myTask = JSON.parse(localStorage.getItem('myTask')) || [];
 
-    // Act the Add Task
+    // Act
     addmyTask('clicked');
     const newTask = JSON.parse(localStorage.getItem('myTask'));
     const newTaskLength = newTask.length;
 
-    // Assert the Add Task
+    // Assert
     expect(newTaskLength).toBe(myTask.length + 1);
     expect(newTask[newTaskLength - 1].description).toBe('new task');
   });
 
-  // Start for Remove Items
+  // Start (Remove)
   test('Removing item', () => {
-    // Arrange the lines
+    // Arrange 
     const myTask = JSON.parse(localStorage.getItem('myTask'));
 
-    // Act the Remove task
+    // Act
     deletemyTask(1);
     const newTask = JSON.parse(localStorage.getItem('myTask'));
 
-    // Assert the Remove task
+    // Assert
     expect(newTask.length).toBe(myTask.length - 1);
   });
 });
